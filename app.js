1d81f96fa06723500b14c670da317271e0396e0e
nick lachance <nicholasplachance@gmail.com>
2019-10-09 20:42:48 -0500

    Hopefully reset and got everything working again

--- app.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector("#hamburger-icon");
-const navUl = document.querySelector("nav ul")
-
-hamburger.addEventListener("click", function() {
-  navUl.classList.toggle("is-hidden--mobile");
-})
