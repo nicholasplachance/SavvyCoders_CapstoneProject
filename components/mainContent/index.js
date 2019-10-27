dfe451ee9ae85fb80732d6636cc656fd9799bb47
nick lachance <nicholasplachance@gmail.com>
2019-10-27 15:08:47 -0500

    Add files to breakup and refactor state and functionality

---
+++ components/mainContent/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home";
+export { default as Movies } from "./Movies";
+export { default as Comics } from "./Comics";
+export { default as Recommendations } from "./Recommendations";
