17427db38fefc84a4b2b4e3c86f93b068e46cf3d
nick lachance <nicholasplachance@gmail.com>
2019-11-05 15:08:14 -0600

    Add comic html cards to comics page

--- components/mainContent/Comics.js
+++ components/mainContent/Comics.js
@@ -10,19 +10,20 @@ function generateArticlesHTML(comics) {
         author,
         illustrator,
         cover,
-        url
+        url,
+        plot
       }) => `<section class="movie-sections"><div class="card-container movies-container grid-container">
             <div class="flex-container--mobile---column card-labels">
                 <h3 class="movie-title">${title}</h3>
                 <h4 class="movie-release--year">${author} | ${year}</h4>
                 <h4 class="movie-actors">${illustrator}</h4>
                 <h4>${category}</h4>
-                <h4 >Buy the comic here!</h4>
-                <a href="url" >${url}</h4>
+                <p class="movie-plot">${plot}</p>
+
             </div>
             <div class="flex-container--mobile---row card-images">
               <img class="movie-poster"  src="${cover}" alt="">
-              <li><a href="./comics" data-navigo class="read-more">${category}</a></li>
+                <a href="${url}"  target="_blank" class="read-more"  >Buy comic!</a>
             </div>
           </div>
           </section>
--- store/Comics.js
+++ store/Comics.js
@@ -8,6 +8,39 @@ export default {
     author: "Frank Miller",
     illustrator: "Frank Miller",
     cover: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/330474/330474._SX360_CLs%7C360,553%7Ccu/cmx-cu-sash-lg.png%7C0,0,361,554%20208,401,152,152_QL80_TTD_.jpg",
-    url: "shorturl.at/jmn08"
-  }]
+    url: "https://www.comixology.com/Batman-The-Dark-Knight-Returns-30th-Anniversary-Edition/digital-comic/330474?ref=c2VyaWVzL3ZpZXcvZGVza3RvcC9ncmlkTGlzdC9jb21peG9sb2d5VW5saW1pdGVk",
+    plot: "Writer/artist Frank Miller completely reinvents the legend of Batman in this saga of a near-future Gotham City gone to rot, 10 years after the Dark Knight's retirement. Forced to take action, the Dark Knight returns in a blaze of fury, taking on a whole new generation of criminals and matching their level of violence. He is soon joined by a new Robin--a girl named Carrie Kelley--who proves to be just as invaluable as her predecessors."
+  },
+  {
+    title: "Infinity Gauntlet",
+    category: "Avengers",
+    year: "1991",
+    author: "Jim Starlin",
+    illustrator: "George Perez",
+    cover: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/15730/15730._SX360_QL80_TTD_.jpg",
+    url: "https://www.comixology.com/Infinity-Gauntlet/digital-comic/15730?ref=c2VhcmNoL2luZGV4L2Rlc2t0b3Avc2xpZGVyTGlzdC90b3BSZXN1bHRzU2xpZGVy",
+    plot: "Collects Infinity Gauntlet (1991) #1-6. The Mad Titan Thanos has seized control of Infinity Gauntlet and with it near-omnipotent power! Who can stop this deadly new overlord? All of Marvel's top heroes star in this epic of cosmic proportions!"
+  },
+  {
+    title: "Infinite Crisis",
+    category: "Justice League",
+    year: "2005",
+    author: "Geoff Johns",
+    illustrator: "Phill Jimenez",
+    cover: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/48418/DIG010108_1._SX360_QL80_TTD_.jpg",
+    url: "https://www.comixology.com/Infinite-Crisis-2005-2006/digital-comic/48418?ref=c2VhcmNoL2luZGV4L2Rlc2t0b3Avc2xpZGVyTGlzdC90b3BSZXN1bHRzU2xpZGVy",
+    plot: "OMAC robots are rampaging, magic is dying, villains are uniting, and a war is raging in space. And in the middle of it all, a critical moment has divided Earth's three greatest heroes: Superman, Batman and Wonder Woman. It's the DCU's darkest day, and long-lost heroes from the past have returned to make things right in the universe...at any cost. Heroes will live, heroes will die, and the DCU will never be the same again!"
+  },
+  {
+    title: "Civil War",
+    category: "Avengers",
+    year: "2006",
+    author: "Mark Millar",
+    illustrator: "Steve McNiven",
+    cover: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/19456/19456._SX360_CLs%7C360,553%7Ccu/cmx-cu-sash-lg.png%7C0,0,361,554%20208,401,152,152_QL80_TTD_.jpg",
+    url: "https://www.comixology.com/Civil-War/digital-comic/19456?ref=c2VhcmNoL2luZGV4L2Rlc2t0b3Avc2xpZGVyTGlzdC90b3BSZXN1bHRzU2xpZGVy",
+    plot: "Whose side are you on? A conflict is brewing that threatens to pit friend against friend, brother against brother and all it will take is a single misstep to cost thousands their lives and ignite the fuse! As the war claims its first victims, no one is safe as teams, friendships and families begin to fall apart and the Marvel Universe super heroes go to war against each other."
+  },
+
+]
 }
